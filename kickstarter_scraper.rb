254acf329c5379bb059061f5b85ed5ca90455a07
Sam D <samidiaz3@gmail.com>
2021-04-01 00:22:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,11 @@
 require 'nokogiri'
-
+require 'pry'
 
 def create_project_hash
   # This just opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
